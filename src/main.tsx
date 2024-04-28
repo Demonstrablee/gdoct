ed2d6f99722e93a5cc7c6b430c8138274f4ccc09
Demonstrablee <unsuleed@gmail.com>
2024-04-28 19:10:48 -0400

    moved routing to the main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import'./App.css'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import FinalD from './pages/FinalD.tsx'
+import Home from './pages/Home.tsx'
+// Routing must be in this main file
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={'/gdoct/'}>
+        <Routes>
+          <Route path="/" element = {<Home/>} />  
+          <Route path="/dia" element={<FinalD />} />
+        </Routes>
+      </BrowserRouter>
+
   </React.StrictMode>,
 )
